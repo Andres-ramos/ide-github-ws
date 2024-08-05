ca4f46cadbadeb83cbbe50f6e40759ccb2dc8c47
Andres-ramos <ramosandres443@gmail.com>
2024-08-05 10:09:13 -0400

    Workshop changes

--- webapp_tutorial/etl.py
+++ webapp_tutorial/etl.py
@@ -7,6 +7,26 @@ from webapp.db import get_db
 app = create_app()
 
 def run_etl():
-    #TODO: Implement
+    #Extract 
+    # lat = 18.403958942814228
+    # lon = -66.04510186605337
+    # c = Scrapper()
+    # data = c.fetch_data(lat, lon)
+    
+    # #Transform
+    # temps = data["temperature"]
+    # dates = data["time"]
+
+    # #load
+    # with app.app_context():
+    #     db = get_db()
+    #     for temp, date in zip(temps, dates):
+    #         db.execute(
+    #             'INSERT INTO temperature (lat, lon, temperature, time)'
+    #             ' VALUES (?, ?, ?, ?)',
+    #             (lat, lon, temp, date)
+    #         )
+    #     db.commit()
+    # return 
     return 
 run_etl()
